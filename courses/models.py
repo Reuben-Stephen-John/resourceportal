ecd2f0a9837a0b5838a05be65bdac5e161078440
Reuben-Stephen-John <reuben.sj2003@gmail.com>
2023-05-09 07:25:19 +0530

    updated models

--- courses/models.py
+++ courses/models.py
@@ -1,18 +1,19 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 from .choices import *
 # Create your models here.
 class Course(models.Model):
     course_code=models.CharField(max_length=10)
     course_title=models.CharField(max_length=100)
     school=models.CharField(max_length=100,choices=CHOICE_SCHOOL)
-    credits_no=models.IntegerField()
+    credits_no=models.FloatField(validators=[MaxValueValidator(4),MinValueValidator(1)])
 
 class Module(models.Model):
     course=models.ForeignKey(Course,on_delete=models.CASCADE)
     module_name=models.CharField(max_length=100)
-    module_no=models.IntegerField()
+    module_no=models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(1)])
     topics=models.CharField(max_length=500)
-    links=models.URLField()
-    notes=models.URLField()
+    links=models.TextField(null=False)
+    notes=models.TextField(null=False)
