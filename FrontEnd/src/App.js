3a8c8a5f688ada7de3be475eca9eaf8d11f901de
Yash Jain <yashphulfagar@gmail.com>
2023-05-20 18:16:15 +0530

    fetch commit

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -3,8 +3,20 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './page/homepage.page'
 import CoursesPage from './page/courses.page'
 import SearchforCourses from './page/searchCourses.page'
+import { useEffect, useState } from 'react'
 
 const App = () => {
+  const [courses, setCourses] = useState([])
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/courses')
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error))
+  }, [])
+
+  console.log(courses)
+
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
--- resourceportal/settings.py
+++ resourceportal/settings.py
@@ -38,17 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'courses',
+    'corsheaders',
     'rest_framework',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'resourceportal.urls'
@@ -123,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
